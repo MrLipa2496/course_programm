556a01eec2a59c1780a3954dc573bc5baa8b2f22
MrLipa2496 <sasalipa216@gmail.com>
2024-12-13 13:24:01 +0200

    Add home page & style

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import homeBg from '../../../img/home-bg.jpg';
+import styles from './Home.module.sass';
+import TourSearch from './TourSearch';
+import PopularDestinations from './PopularDestinations';
+
+function Home () {
+  return (
+    <>
+      <div className={styles.container}>
+        <img className={styles.homeBg} src={homeBg} alt={homeBg} />
+
+        <div className={styles.titleWrapper}>
+          <h1 className={styles.homeTitle}>Explore Your Dream Destinations</h1>
+          <p className={styles.subTitle}>
+            Discover breathtaking landscapes and cultural treasures with our
+            curated travel experiences.
+          </p>
+          <p className={styles.bookingLink}>
+            Start your journey today!
+            <NavLink className={styles.link} to='/booking'>
+              Booking
+            </NavLink>
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <TourSearch />
+      </div>
+
+      <div>
+        <PopularDestinations />
+      </div>
+    </>
+  );
+}
+
+export default Home;
