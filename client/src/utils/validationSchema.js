f4c61fc7c9876237de51b1d5e90f022d27d05b7a
MrLipa2496 <sasalipa216@gmail.com>
2024-12-13 19:02:37 +0200

    Update TourSearchForm validation

--- client/src/utils/validationSchema.js
+++ client/src/utils/validationSchema.js
@@ -4,35 +4,23 @@ export const TOUR_SEARCH_FORM_VALIDATION = yup.object({
   destination: yup
     .string()
     .trim()
-    .min(8, 'The destination must contain at least 8 characters.')
+    .min(4, 'The destination must contain at least 8 characters.')
     .max(30, 'The destination must contain no more than 30 characters.')
-    .matches(/^[A-Z]/, 'Country/city must start with a capital letter')
-    .required('Destination is required.'),
+    .matches(/^[A-Z]/, 'Country/city must start with a capital letter'),
   tourType: yup
     .string()
-    .oneOf(['leisure', 'excursion', 'business'], 'Invalid tour type')
-    .required('Tour type is required.'),
-  startDate: yup
-    .date()
-    .min(new Date(), 'Start date cannot be in the past.')
-    .required('Start date is required.'),
+    .oneOf(
+      ['Leisure', 'Excursion', 'Business', 'Adventure'],
+      'Invalid tour type'
+    ),
+  startDate: yup.date().min(new Date(), 'Start date cannot be in the past.'),
   endDate: yup
     .date()
-    .min(yup.ref('startDate'), 'End date cannot be before start date.')
-    .required('End date is required.'),
+    .min(yup.ref('startDate'), 'End date cannot be before start date.'),
   budget: yup
     .number()
     .positive('Budget must be a positive number.')
-    .integer('Budget must be an integer.')
-    .required('Budget is required.'),
-  transport: yup
-    .string()
-    .oneOf(['plane', 'train', 'bus'], 'Invalid transport type')
-    .required('Transport type is required.'),
-  hotelClass: yup
-    .string()
-    .oneOf(['3-star', '4-star', '5-star'], 'Invalid hotel class')
-    .required('Hotel class is required.'),
+    .integer('Budget must be an integer.'),
 });
 
 const NAME_VALIDATION = yup
