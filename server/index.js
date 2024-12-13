af3799160ddf908bd603ff03f18db8516be9c913
MrLipa2496 <sasalipa216@gmail.com>
2024-12-13 13:39:05 +0200

    Add host & port

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 5001;
+const HOST = process.env.HOST || '127.0.0.1';
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, HOST, () =>
+  console.log(`Server is listening http://${HOST}:${PORT}`)
+);
