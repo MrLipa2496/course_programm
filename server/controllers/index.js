cdef17c4cfbdde782ff1c3e1e04693bae13cacaf
MrLipa2496 <sasalipa216@gmail.com>
2024-12-13 13:41:57 +0200

    Expoert tours & bookings &  transports & hotels & receipt controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,11 @@
+module.exports.toursController = require('./toursController');
+
+module.exports.transportsController = require('./transportsController');
+
+module.exports.hotelsController = require('./hotelsController');
+
+module.exports.bookingsController = require('./bookingsController');
+
+module.exports.clientsController = require('./clientsController');
+
+module.exports.receiptController = require('./receiptController');
