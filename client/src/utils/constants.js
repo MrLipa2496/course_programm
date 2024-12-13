9a13b0229f44a99c909860fe1a18e912a98193d5
MrLipa2496 <sasalipa216@gmail.com>
2024-12-13 13:36:25 +0200

    Add constants

---
+++ client/src/utils/constants.js
@@ -0,0 +1,9 @@
+const CONSTANTS = {
+  TOURS_SLICE_NAME: 'tours',
+  TRANSPORTATION_SLICE_NAME: 'transportations',
+  HOTELS_SLICE_NAME: 'hotels',
+  CLIENTS_SLICE_NAME: 'clients',
+  BOOKINGS_SLICE_NAME: 'bookings',
+};
+
+export default CONSTANTS;
